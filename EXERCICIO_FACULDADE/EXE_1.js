181046e3f0ea250cb3770beaab36ff7521e9cc8c
fat83dotcom <fernando.quimic@gmail.com>
2022-07-01 14:01:39 -0300

    ajustes

--- EXERCICIO_FACULDADE/EXE_1.js
+++ EXERCICIO_FACULDADE/EXE_1.js
@@ -27,31 +27,44 @@ function sinal(valor) {
     let sinalHtml = document.getElementById('sinalOperacao')
     sinalHtml.innerHTML = valor
 }
+function sinaisOperacao(params){
+    if (document.getElementById('adicao').checked){
+        sinal('+')
+    }
+    else if (document.getElementById("subtracao").checked){
+        sinal('-')
+    }
+    else if (document.getElementById("divisao").checked){
+        sinal('/')
+    }
+    else if (document.getElementById("multiplicacao").checked){
+        sinal('X')
+    }
+}
 function executar() {
     let numeroA = parseFloat(document.getElementById("numeroA").value)
     let numeroB = parseFloat(document.getElementById("numeroB").value)
     if (document.getElementById('adicao').checked) {
         resultado = adicao(numeroA, numeroB)
-        sinal('+')
         imprimeResultado(resultado)
     }
     else if (document.getElementById("subtracao").checked) {
         resultado = subtracao(numeroA, numeroB)
-        sinal('-')
         imprimeResultado(resultado)
     }
     else if (document.getElementById("divisao").checked) {
         resultado = divisao(numeroA, numeroB)
-        sinal('/')
         imprimeResultado(resultado)
     }
     else if (document.getElementById("multiplicacao").checked) {
         resultado = multiplicacao(numeroA, numeroB)
-        sinal('X')
         imprimeResultado(resultado)
     }
     else {
         imprimeResultado("Escolha uma operação") 
     }
 }
+setInterval(function() {    
+    sinaisOperacao()
+}, 1)
 // console.log(adicao(2, 4))
