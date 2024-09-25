08a2edca81832cffdfef8ad1bf103c2175b42c30
fat83dotcom <fernando.quimic@gmail.com>
2024-09-24 21:13:59 -0300

    Atualização do curso.

--- aula_51/gravar_arquivo.js
+++ aula_51/gravar_arquivo.js
@@ -3,19 +3,18 @@ const fs = require('fs')
 
 const text = 'Aqui tem conteudo'
 
-fs.writeFile('file.txt', text, {flag: 'a+'}, err => {
+fs.writeFileSync('file.txt', text, {flag: 'a+'}, err => {
     if (err) {
         console.log(err);
         return
     }
 });
 
-for (let i = 0; i < 500; i++) {
-    fs.writeFile('file.txt', `${i}\n`, {flag: 'a+'}, err => {
+for (let i = 0; i < 500000; i++) {
+    fs.writeFileSync('file.txt', `${i}\n`, {flag: 'a+'}, err => {
         if (err) {
             console.log(err);
             return
         }
     });
-    
 }
